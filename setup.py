6b336fcce0a1fd769318cab53b15579e2d573819
nikk-nikaznan <nik.k.nikaznan@outlook.com>
2025-05-30 23:05:27 +0100

    change the package name for pytest

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from glob import glob
 
-package_name = 'eeg_msgs'
+package_name = 'sense_msgs'
 
 setup(
     name=package_name,
@@ -10,7 +10,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/msg', glob('eeg_msgs/msg/*.msg')),
+        ('share/' + package_name + '/msg', glob('sense_msgs/msg/*.msg')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
